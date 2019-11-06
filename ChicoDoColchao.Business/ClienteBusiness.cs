f76515d818e1d4378258d5ced1c47110cd28e22a
Fabio Henrique <fabiohenrique86@gmail.com>
2019-11-05 22:35:25 -0300

    1. validar quantidade de dígitos CPF e CNPJ

--- ChicoDoColchao.Business/ClienteBusiness.cs
+++ ChicoDoColchao.Business/ClienteBusiness.cs
@@ -22,107 +22,77 @@ namespace ChicoDoColchao.Business
         private void ValidarIncluir(ClienteDao clienteDao)
         {
             if (clienteDao == null)
-            {
                 throw new BusinessException("Cliente é obrigatório");
-            }
 
             if (string.IsNullOrEmpty(clienteDao.Cpf) && string.IsNullOrEmpty(clienteDao.Cnpj))
-            {
-                throw new BusinessException("Cpf ou CNPJ é obrigatório");
-            }
+                throw new BusinessException("CPF ou CNPJ é obrigatório");
             else if (!string.IsNullOrEmpty(clienteDao.Cpf) && !string.IsNullOrEmpty(clienteDao.Cnpj))
-            {
-                throw new BusinessException("Informe Cpf ou CNPJ");
-            }
+                throw new BusinessException("Informe CPF ou CNPJ");
 
             if (!string.IsNullOrEmpty(clienteDao.Cpf))
             {
+                if (clienteDao.Cpf.Replace(".", "").Replace("-", "").Length != 11)
+                    throw new BusinessException("CPF deve ter 11 dígitos");
+
                 if (string.IsNullOrEmpty(clienteDao.Nome))
-                {
                     throw new BusinessException("Nome é obrigatório");
-                }
 
                 if (clienteDao.DataNascimento == null || clienteDao.DataNascimento == DateTime.MinValue)
-                {
                     throw new BusinessException("Data de Nascimento é obrigatório");
-                }
             }
             else
             {
+                if (clienteDao.Cnpj.Replace(".", "").Replace("-", "").Replace("/", "").Length != 14)
+                    throw new BusinessException("CNPJ deve ter 14 dígitos");
+
                 if (string.IsNullOrEmpty(clienteDao.NomeFantasia))
-                {
                     throw new BusinessException("Nome Fantasia é obrigatório");
-                }
 
                 if (string.IsNullOrEmpty(clienteDao.RazaoSocial))
-                {
                     throw new BusinessException("Razão Social é obrigatório");
-                }
             }
 
             if (string.IsNullOrEmpty(clienteDao.Email))
-            {
                 throw new BusinessException("E-mail é obrigatório");
-            }
 
             if (string.IsNullOrEmpty(clienteDao.TelefoneResidencial) && string.IsNullOrEmpty(clienteDao.TelefoneResidencial2) && string.IsNullOrEmpty(clienteDao.TelefoneCelular) && string.IsNullOrEmpty(clienteDao.TelefoneCelular2))
-            {
                 throw new BusinessException("Um telefone é obrigatório");
-            }
 
             if (clienteDao.EstadoDao == null || clienteDao.EstadoDao.Count <= 0 || clienteDao.EstadoDao.FirstOrDefault().EstadoID <= 0)
-            {
                 throw new BusinessException("Estado é obrigatório");
-            }
 
             if (string.IsNullOrEmpty(clienteDao.Cidade))
-            {
                 throw new BusinessException("Cidade é obrigatório");
-            }
 
             if (string.IsNullOrEmpty(clienteDao.Logradouro))
-            {
                 throw new BusinessException("Logradouro é obrigatório");
-            }
 
             if (string.IsNullOrEmpty(clienteDao.Bairro))
-            {
                 throw new BusinessException("Bairro é obrigatório");
-            }
 
             if (clienteDao.Numero == null || clienteDao.Numero <= 0)
-            {
                 throw new BusinessException("Número é obrigatório");
-            }
 
             if (clienteRepository.Listar(new Cliente()
             {
                 Cpf = string.IsNullOrEmpty(clienteDao.Cpf) ? string.Empty : clienteDao.Cpf.Replace(".", "").Replace("-", ""),
                 Cnpj = string.IsNullOrEmpty(clienteDao.Cnpj) ? string.Empty : clienteDao.Cnpj.Replace(".", "").Replace("-", "").Replace("/", ""),
             }).FirstOrDefault() != null)
-            {
                 throw new BusinessException(string.Format("CPF ou CNPJ {0} já cadastrado", string.IsNullOrEmpty(clienteDao.Cpf) ? clienteDao.Cnpj : clienteDao.Cpf));
-            }
         }
 
         private void ValidarAtualizar(ClienteDao clienteDao, out Cliente cliente)
         {
             if (clienteDao == null)
-            {
                 throw new BusinessException("Cliente é obrigatório");
-            }
 
             if (clienteDao.ClienteID <= 0)
-            {
                 throw new BusinessException("ClienteID é obrigatório");
-            }
 
             cliente = clienteRepository.Listar(new Cliente() { ClienteID = clienteDao.ClienteID }).FirstOrDefault();
 
             if (cliente == null)
-            {
                 throw new BusinessException(string.Format("Cliente {0} não encontrado", clienteDao.ClienteID));
-            }
         }
 
         public List<ClienteDao> Listar(ClienteDao clienteDao)
@@ -137,7 +107,7 @@ namespace ChicoDoColchao.Business
             }
             catch (Exception ex)
             {
-                
+
                 logRepository.Incluir(new Log() { Descricao = ex.ToString(), DataHora = DateTime.Now });
 
                 throw ex;
@@ -156,7 +126,7 @@ namespace ChicoDoColchao.Business
             }
             catch (Exception ex)
             {
-                
+
                 logRepository.Incluir(new Log() { Descricao = ex.ToString(), DataHora = DateTime.Now });
 
                 throw ex;
@@ -177,7 +147,7 @@ namespace ChicoDoColchao.Business
             }
             catch (Exception ex)
             {
-                
+
                 logRepository.Incluir(new Log() { Descricao = ex.ToString(), DataHora = DateTime.Now });
 
                 throw ex;
@@ -295,7 +265,7 @@ namespace ChicoDoColchao.Business
             }
             catch (Exception ex)
             {
-                
+
                 logRepository.Incluir(new Log() { Descricao = ex.ToString(), DataHora = DateTime.Now });
 
                 throw ex;
