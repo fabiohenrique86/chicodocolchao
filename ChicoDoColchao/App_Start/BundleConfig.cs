7c87ada47c38fa9d2be7a327169693c7dcf40ecd
Fabio Henrique <fabiohenrique86@gmail.com>
2021-02-24 17:57:01 -0300

    alterando bundle

--- ChicoDoColchao/App_Start/BundleConfig.cs
+++ ChicoDoColchao/App_Start/BundleConfig.cs
@@ -13,7 +13,7 @@ namespace ChicoDoColchao
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include("~/Scripts/jquery.validate.min.js"));
             bundles.Add(new ScriptBundle("~/bundles/globalize").Include("~/Scripts/jquery.globalize/globalize.js",
                                                                         "~/Scripts/jquery.globalize/cultures/globalize.cultures.js",
-                                                                        "~/Scripts/jquery.globalize/cultures/globalize.culture.pt-BR.js"));            
+                                                                        "~/Scripts/jquery.globalize/cultures/globalize.culture.pt-BR.js"));
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap-3.3.6.min.js"));
             bundles.Add(new ScriptBundle("~/bundles/mask").Include("~/Scripts/mask.js"));
             bundles.Add(new ScriptBundle("~/bundles/datatables").Include("~/Scripts/jquery.dataTables-1.10.12.min.js"));
@@ -24,13 +24,16 @@ namespace ChicoDoColchao
 
             // CSS
             bundles.Add(new StyleBundle("~/bundles/css").Include("~/Content/bootstrap-3.3.6.min.css",
-                                                                "~/Content/jquery.ui-1.11.4.css",
+                                                                //"~/Content/jquery.ui-1.11.4.css",
                                                                 "~/Content/jquery.dataTables.css",
                                                                 "~/Content/modern-business.css",
                                                                 "~/Content/font-awesome.min.css",
                                                                 "~/Content/fullcalendar.min.css"//,
-                                                                //"~/Content/fullcalendar.print.min.css"
+                                                                                                //"~/Content/fullcalendar.print.min.css"
                                                                 ));
+
+            // Jquery UI - images
+            bundles.Add(new StyleBundle("~/bundles/jqueryuicss").Include("~/Content/jquery.ui-1.11.4.css", new CssRewriteUrlTransform()));
         }
     }
 }
